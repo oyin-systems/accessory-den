1e9ca801291e00662bcf450ca1254d665be1ba37
Oyindamola Abolaji <78956440+oyin-systems@users.noreply.github.com>
2024-07-08 11:46:00 +0100

    make cart icon clickable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,8 +49,8 @@ const Header = () => {
           </div>
 
           <div className="flex mr-10 md:mr-2">
-            <Image src="/images/cart.png" alt="cart" width={24} height={24} className="mr-2"/>
-            <Link href="/cart" className="hover:text-gray-400">Cart</Link>
+          <Link href="/cart"><Image src="/images/cart.png" alt="cart" width={24} height={24} className="mr-2"/></Link>
+          <Link href="/cart" className="hover:text-gray-400">Cart</Link>
           </div>
         </div>
       </div>
